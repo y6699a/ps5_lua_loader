1e1b78b8b484fc0fe54c0effd6052c338c86b9c1
n0llptr <viljosel@proton.me>
2024-12-07 20:45:26 +0200

    Receive multiple signals per payload

--- send_lua.py
+++ send_lua.py
@@ -20,28 +20,30 @@ def send_payload(ip, port, filepath):
         sock.sendall(size + data)
 
         response = []
+        crash_flag = False
         while True:
             chunk = sock.recv(4096)
             if not chunk:
                 break
             response.append(chunk)
-
-            if len(chunk) == 8 or len(chunk) == 16:
-                break
-
-        response = b''.join(response)
-        if len(response) == 16:
-            crash_code_data = struct.unpack("<Q", response[:8])[0]
-            crash_code = signals.get(crash_code_data, f"Unknown signal code {crash_code_data}")
             
-            crash_address_data = struct.unpack("<Q", response[8:])[0]
-            crash_address = f"0x{crash_address_data:016x}"
-            
-            print(f"{crash_code} at {crash_address}")
-        elif len(response) == 8:
-            crash_address = binascii.hexlify(bytes(reversed(response))).decode('utf-8')
-            print(f"Crash at 0x{crash_address}")
-        else:
+            if len(chunk) == 8:
+                crash_flag = True
+                crash_address = binascii.hexlify(bytes(reversed(response))).decode('utf-8')
+                print(f"Crash at 0x{crash_address}")
+
+            if len(chunk) == 16:
+                crash_flag = True
+                crash_code_data = struct.unpack("<Q", chunk[:8])[0]
+                crash_code = signals.get(crash_code_data, f"Unknown signal code {crash_code_data}")
+                
+                crash_address_data = struct.unpack("<Q", chunk[8:])[0]
+                crash_address = f"0x{crash_address_data:016x}"
+                
+                print(f"{crash_code} at {crash_address}")
+
+        if not crash_flag:
+            response = b''.join(response)
             print(response.decode("utf-8"))
 
 def main():
