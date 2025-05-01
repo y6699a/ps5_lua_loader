fe541a977c0b473f1e266d8c501c680f82da7551
najdek <4890607+najdek@users.noreply.github.com>
2025-05-01 12:40:22 +0200

    elf_sender: cleanup

--- savedata/elf_sender.lua
+++ savedata/elf_sender.lua
@@ -4,23 +4,14 @@
 -- This script sends an ELF file to a local server running on port 9021.
 -- 
 
-
 function sceNetSend(sockfd, buf, len, flags, addr, addrlen)
     return syscall.sendto(sockfd, buf, len, flags, addr, addrlen):tonumber()
-end
-function sceNetConnect(sockfd, addr, addrlen)
-    return syscall.connect(sockfd, addr, addrlen):tonumber()
 end
 function sceNetSocket(domain, type, protocol)
     return syscall.socket(domain, type, protocol):tonumber()
 end
 function sceNetSocketClose(sockfd)
     return syscall.close(sockfd):tonumber()
-end
-
-function ip_to_dword(ip)
-    local a, b, c, d = ip:match("(%d+)%.(%d+)%.(%d+)%.(%d+)")
-    return bit32.lshift(tonumber(a), 24) + bit32.lshift(tonumber(b), 16) + bit32.lshift(tonumber(c), 8) + tonumber(d)
 end
 function htons(port)
     return bit32.bor(bit32.lshift(port, 8), bit32.rshift(port, 8)) % 0x10000
@@ -29,9 +20,18 @@ end
 elf_sender = {}
 elf_sender.__index = elf_sender
 
+AF_INET = 2
+SOCK_STREAM = 1
+
+
 function elf_sender:load_from_file(filepath)
-    if not file_exists(filepath) then
-        errorf("file does not exist: %s", filepath)
+
+    if file_exists(filepath) then
+        print("Loading elf from:", filepath)
+        send_ps_notification("Loading elf from: \n" .. filepath)
+    else
+        print("[-] File not found:", filepath)
+        send_ps_notification("[-] File not found: \n" .. filepath)
     end
 
     local self = setmetatable({}, elf_sender)
@@ -49,11 +49,11 @@ syscall.resolve(
     }
 )
 
-function send_elf_to_localhost(filepath)
+function elf_sender:send_to_localhost(filepath, port)
     local elf = elf_sender:load_from_file(filepath)
     local elf_data = elf.elf_data
 
-    local sockfd = sceNetSocket(2, 1, 0)
+    local sockfd = sceNetSocket(AF_INET, SOCK_STREAM, 0)
     print("Socket fd:", sockfd)
     assert(sockfd >= 0, "socket creation failed")
 
@@ -69,8 +69,8 @@ function send_elf_to_localhost(filepath)
     local sockaddr = memory.alloc(16)
 
     memory.write_byte(sockaddr + 0, 16)
-    memory.write_byte(sockaddr + 1, 2) -- AF_INET
-    memory.write_word(sockaddr + 2, htons(9021)) -- Port
+    memory.write_byte(sockaddr + 1, AF_INET)
+    memory.write_word(sockaddr + 2, htons(port))
 
     memory.write_byte(sockaddr + 4, 0x7F) -- 127
     memory.write_byte(sockaddr + 5, 0x00) -- 0
@@ -81,15 +81,13 @@ function send_elf_to_localhost(filepath)
     memory.write_buffer(buf, elf_data)
 
     local total_sent = sceNetSend(sockfd, buf, #elf_data, 0, sockaddr, 16)
-    print("Sendto result:", total_sent)
     sceNetSocketClose(sockfd)
+    if total_sent < 0 then
+        print("[-] error sending elf data to localhost")
+        return
+    end
     print(string.format("Successfully sent %d bytes to loader", total_sent))
 end
 
 local filetoload = "/mnt/usb0/test.elf"
-if file_exists(filetoload) then
-    send_elf_to_localhost(filetoload)
-else
-    print("[-] File not found: " .. filetoload)
-end
-
+elf_sender:send_to_localhost(filetoload, 9021)
